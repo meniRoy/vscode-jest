b1b4b237b8ebb893c2225282675a367360f8730a
meni <meni.roytenburd@gmail.com>
2023-07-05 18:38:52 +0300

    run with jest

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,17 +3,17 @@ console.log('global meni',(global as any).meni);
 
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-// describe('Extension Test Suite', () => {
-// 	it('Sample test', async() => {
-// 		vscode.window.showInformationMessage('Hello World from yotest!');
-// 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-// 	})
-// });
-
-suite('Extension Test Suite', () => {
-	test('Sample test', async() => {
+describe('Extension Test Suite', () => {
+	it('Sample test', async() => {
 		vscode.window.showInformationMessage('Hello World from yotest!');
-		await new Promise(res=>setTimeout(res,3000))
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	}).timeout(10000)
+	})
 });
+
+// suite('Extension Test Suite', () => {
+// 	test('Sample test', async() => {
+// 		vscode.window.showInformationMessage('Hello World from yotest!');
+// 		await new Promise(res=>setTimeout(res,3000))
+// 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+// 	}).timeout(10000)
+// });
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -7,57 +7,57 @@ import * as vscode from 'vscode';
 console.log('index.ts cache', Object.keys(require.cache).length);
 console.log('index.ts global meni',(global as any).meni);
 
-// export async function run(){
-// 	const testsRoot = path.resolve(__dirname, '../../');
+export async function run(){
+	const testsRoot = path.resolve(__dirname, '../../');
 
-//   const jestOptions = {
-//     roots: [testsRoot],
-//     testMatch: ['**/*.test.js'],
-//     runInBand:true
-//   };
+  const jestOptions = {
+    roots: [testsRoot],
+    testMatch: ['**/*.test.js'],
+    runInBand:true
+  };
 
-//   // Add any additional Jest configuration options as needed
+  // Add any additional Jest configuration options as needed
 
-//   try {
-//     // Run the tests with Jest
-// 	await jest.runCLI(jestOptions as any,jestOptions.roots);
-//   } catch (err) {
-//     console.error(err);
-//     throw err;
-//   }
-// }
-
-export function run(): Promise<void> {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
-
-	const testsRoot = path.resolve(__dirname, '..');
-
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+  try {
+    // Run the tests with Jest
+	await jest.runCLI(jestOptions as any,jestOptions.roots);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
+
+// export function run(): Promise<void> {
+// 	// Create the mocha test
+// 	const mocha = new Mocha({
+// 		ui: 'tdd',
+// 		color: true
+// 	});
+
+// 	const testsRoot = path.resolve(__dirname, '..');
+
+// 	return new Promise((c, e) => {
+// 		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+// 			if (err) {
+// 				return e(err);
+// 			}
+
+// 			// Add files to the test suite
+// 			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+// 			try {
+// 				// Run the mocha test
+// 				mocha.run(failures => {
+// 					if (failures > 0) {
+// 						e(new Error(`${failures} tests failed.`));
+// 					} else {
+// 						c();
+// 					}
+// 				});
+// 			} catch (err) {
+// 				console.error(err);
+// 				e(err);
+// 			}
+// 		});
+// 	});
+// }
